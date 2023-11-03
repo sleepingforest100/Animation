c3f159e451c2fdfb622f81c05937667c4ffe19dc
Madina <123440519+sleepingforest100@users.noreply.github.com>
2023-11-03 09:12:54 +0600

    feature: fixing errors

--- app/src/main/java/kz/just_code/animationapp/MainActivity.kt
+++ app/src/main/java/kz/just_code/animationapp/MainActivity.kt
@@ -1,16 +1,19 @@
 package kz.just_code.animationapp
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentActivity
 import kz.just_code.animationapp.databinding.ActivityMainBinding
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
+
 
 
